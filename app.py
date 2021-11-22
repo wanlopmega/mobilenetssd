1b6182a8ad9f57d1ae71582aa9862d5081a06e7b
wanlopmega <89174668+wanlopmega@users.noreply.github.com>
2021-11-22 13:54:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def event_handle(event):
         else :
             replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
-      elif msgType == "image":
+    elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
             i = Image.open(BytesIO(message_content.content))
